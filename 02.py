1cab35d54910d1f96e2147477540e6171c89bbdc
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-29 15:48:50 -0500

    finish puzzle 2

--- 02.py
+++ 02.py
@@ -4,6 +4,15 @@ with open('02.txt', 'r') as file:
     data = file.read()
 
 
-count = Counter(data)
-c = count.most_common()[0][0]
-print(c)
+cur = Counter(data).most_common()[0][0]  # P
+res = []
+
+while cur != ';':
+    res.append(cur)
+    count = Counter()
+    for i, c in enumerate(data):
+        if c == cur:
+            count[data[i+1]] += 1
+    cur = count.most_common()[0][0]
+
+print(''.join(res))  # PArietalLobE
