d54bef14e8a6e6abb03481fb91bccb4dbb0eecef
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-29 16:19:11 -0500

    find starting point, finishing points

--- 03.py
+++ 03.py
@@ -4,6 +4,13 @@ import networkx as nx
 with open('03.txt', 'r') as file:
     data = file.read()
 
+DIRS = {
+    'U': (0, -1),
+    'D': (0, 1),
+    'R': (1, 0),
+    'L': (-1, 0),
+}
+
 
 def parse_raw(data):
     strands = {}
@@ -13,8 +20,35 @@ def parse_raw(data):
         moves = x[2].split(',')
         strands[pos] = moves
     strands[(25,95)] = 'R,U,R,D,R,U,R,U,U,L,L,L,U,U,U,U,L,L,D'.split(',')  # my regex missed this one
+    strands[(104,15)] = []  # make this empty list rather than ['']
     return strands
 
 
 strands = parse_raw(data)
-print(len(strands))
+
+
+def find_points(strands):
+    valid, start, finish, walls = set(), set(), set(), set()
+    for k, v in strands.items():
+        valid.add(k)
+        cur = k
+        for d in v:
+            if d == 'F':
+                finish.add(cur)
+            elif d == 'S':
+                start.add(cur)
+            elif d == 'X':
+                walls.add(cur)
+            else:  # d in DIRS
+                x, y = cur
+                dx, dy = DIRS[d]
+                cur = (x+dx, y+dy)
+                valid.add(k)
+    valid |= (start | finish)
+    return valid, start, finish, walls
+
+
+valid, start, finish, walls = find_points(strands)
+# print(start)  # starting point is (2, 2)
+# print(finish)  # length of valid finishes is 11
+
