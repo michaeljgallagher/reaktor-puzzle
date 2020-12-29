93c230d6dd1cf797ba0b41c53ee69d03b2268fbb
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-29 16:01:58 -0500

    parse raw data for puzzle 3

--- 03.py
+++ 03.py
@@ -1,4 +1,20 @@
+import re
+import networkx as nx
+
 with open('03.txt', 'r') as file:
     data = file.read()
 
 
+def parse_raw(data):
+    strands = {}
+    m = re.findall(r'(\d*),(\d*)\s?(\D*)?\n', data)
+    for x in m:
+        pos = (int(x[0]), int(x[1]))
+        moves = x[2].split(',')
+        strands[pos] = moves
+    strands[(25,95)] = 'R,U,R,D,R,U,R,U,U,L,L,L,U,U,U,U,L,L,D'.split(',')  # my regex missed this one
+    return strands
+
+
+strands = parse_raw(data)
+print(len(strands))
