a3329e66096edeb40c29f99520e6f53f4d0ef485
MG <11604739+michaeljgallagher@users.noreply.github.com>
2022-01-30 18:28:21 -0500

    Update formatting for 03.py

--- 03.py
+++ 03.py
@@ -1,25 +1,27 @@
 import re
 import networkx as nx
 
-with open('03.txt', 'r') as file:
+with open("03.txt", "r") as file:
     data = file.read()
 
 DIRS = {
-    'U': (0, -1),
-    'D': (0, 1),
-    'R': (1, 0),
-    'L': (-1, 0),
+    "U": (0, -1),
+    "D": (0, 1),
+    "R": (1, 0),
+    "L": (-1, 0),
 }
 
 
 def parse_raw(data):
     strands = {}
-    m = re.findall(r'(\d*),(\d*)\s?(\D*)?\n', data)
+    m = re.findall(r"(\d*),(\d*)\s?(\D*)?\n", data)
     for x in m:
         pos = (int(x[0]), int(x[1]))
-        moves = x[2].split(',')
+        moves = x[2].split(",")
         strands[pos] = moves
-    strands[(25, 95)] = 'R,U,R,D,R,U,R,U,U,L,L,L,U,U,U,U,L,L,D'.split(',')  # my regex missed this one
+    strands[(25, 95)] = "R,U,R,D,R,U,R,U,U,L,L,L,U,U,U,U,L,L,D".split(
+        ","
+    )  # my regex missed this one
     strands[(104, 15)] = []  # make this empty list rather than ['']
     return strands
 
@@ -33,16 +35,16 @@ def find_points(strands):
         valid.add(k)
         cur = k
         for d in v:
-            if d == 'F':
+            if d == "F":
                 finish.add(cur)
-            elif d == 'S':
+            elif d == "S":
                 start.add(cur)
-            elif d == 'X':
+            elif d == "X":
                 walls.add(cur)
             else:  # d in DIRS
                 x, y = cur
                 dx, dy = DIRS[d]
-                cur = (x+dx, y+dy)
+                cur = (x + dx, y + dy)
                 valid.add(cur)
     return valid, start, finish, walls
 
@@ -55,32 +57,32 @@ valid, start, finish, walls = find_points(strands)
 
 
 def display_graph(valid, start, finish, walls, path=None):
-    grid = [['.' for _ in range(128)] for _ in range(128)]
+    grid = [["." for _ in range(128)] for _ in range(128)]
     for x, y in valid:
-        grid[y][x] = '#'
+        grid[y][x] = "#"
     if path:
         for x, y in path:
             grid[y][x] = chr(9608)
     for x, y in start:
-        grid[y][x] = 'S'
+        grid[y][x] = "S"
     for x, y in finish:
-        grid[y][x] = 'F'
+        grid[y][x] = "F"
     for x, y in walls:
-        grid[y][x] = 'X'
+        grid[y][x] = "X"
     for line in grid:
-        print(''.join(line))
+        print("".join(line))
 
 
 # display_graph(valid, start, finish, walls)
 
 
 def build_graph(valid, start, finish):
-    valid |= (start | finish)
+    valid |= start | finish
     G = nx.Graph()
     for x, y in valid:
         for dx, dy in DIRS.values():
-            if (x+dx, y+dy) in valid:
-                G.add_edge((x+dx, y+dy), (x, y))
+            if (x + dx, y + dy) in valid:
+                G.add_edge((x + dx, y + dy), (x, y))
     return G
 
 
@@ -108,16 +110,16 @@ def convert_to_steps(path):
         if i == 0:
             continue
         x, y = step
-        dx, dy = path[i-1]
-        res.append((x-dx, y-dy))
+        dx, dy = path[i - 1]
+        res.append((x - dx, y - dy))
     return res
 
 
 path = convert_to_steps(path)
 REVERSE = {v: k for k, v in DIRS.items()}
-print(''.join(REVERSE[step] for step in path))
+print("".join(REVERSE[step] for step in path))
 
-'''
+"""
 RRRDDDDRRRUURURURRRRRRRRRRRRRDRRRRRRRRRRRRRRRRDRRDRRRRRRDRRDDRR
 DRDDDDDLDDDDDLDDLLDLDDDLLLLLLLLLDDLLDLLLDDDLDLDDLLLLLLLLLLLDLLL
 LLLLLLLLLLLLLLLDDDDDDDDDDDDDDDDDDDRDDDDRDDDDDRRUUUUURUURRURRRRR
@@ -135,4 +137,4 @@ DRRRRRRRURRRRRDRRRRRURURUURUURRRRUURRRURURRRRRRUUUUUUUURUURUUUU
 UUUUUULLLULLLULULLLLLDLLLULLLLLLLLLLLLUUUUULLLLLLLLDDDLLLDDLLLL
 LLLLLLLLLLLLLLLLUUUUUULUUUUUUUUURUURRUUURRURRRRRRRRRRUUUUUURURR
 RRRRRRRRRRRDDDDDDDRRDRRDDDLLLLLLLLDDRRRDRRDDRRRURRRRRUUURUURRRR
-'''
+"""
